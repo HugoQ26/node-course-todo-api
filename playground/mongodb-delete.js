00446f41c56eaa5eb906d110f0de874ab78c23bd
Marcin Kubiak <hugo26@o2.pl>
2018-05-28 10:24:09 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,44 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj.getTimestamp());
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		console.log('Unable to connect to MongoDb server');
+	} else {
+		console.log('Connected!!');
+	}
+
+	//delete many
+
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((res) => {
+	// 	console.log(res);
+	// });
+	
+	//delete one
+
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+	
+	//find one end delete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// })
+
+
+	var us = db.collection('Users');
+	us.findOneAndDelete({name: 'Gosia'}).then((res) => {
+		console.log(`You just deleted ${res.lastErrorObject.n} input: ${JSON.stringify(res.value, undefined, 2)}`);
+	}).then(() => {
+		us.deleteMany({name: 'Marcin'}).then((res) => {
+			console.log(`You just deleted ${res.result.n} input...`);
+		})
+	})
+
+
+
+	// db.close();
+});
